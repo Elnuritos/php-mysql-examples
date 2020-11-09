b50c3f8d0c77a58254251865c0b5a922ecbf1c78
elnuritos <elnu1990@list.ru>
2020-11-09 15:46:45 +0300

    fields error

--- PHP-MySQL-Login-System/update.php
+++ PHP-MySQL-Login-System/update.php
@@ -2,56 +2,73 @@
 // Initialize the session
 session_start();
 
+
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
-
     exit;
 }else{
-    require_once "config.php";
+    // define first
+    $name = $surnames = $salary = $ycard = "";
+    $rcard = $team = $goals = $assists=""; 
+    require_once "config.php"; 
     if(isset($_POST['id'])&&(!empty(trim($_POST['id'])))){
-        $name = $_POST["name"];
-        $surnames = $_POST["surname"];
-        $salary = $_POST["salary"];
-        $ycard = $_POST["ycard"];
-        $rcard = $_POST["rcard"];
-        $team = $_POST["team"];
-        $goals = $_POST["goals"];
-        $assists = $_POST["assists"];
+        // Error - id was not defined
+        $id = $_POST["id"];
+        //var_dump($id);
+        $name = trim($_POST["name"]);
+        //var_dump($name);
+        $surnames = trim($_POST["surname"]);
+        //var_dump($surnames);
+        $salary = trim($_POST["salary"]);
+        $ycard = trim($_POST["ycard"]);
+        $rcard = trim($_POST["rcard"]);
+        $team = trim($_POST["team"]);
+        $goals = trim($_POST["goals"]);
+        $assists = trim($_POST["assists"]);
+        // a vaidation must be added later
         
-        if (!empty(trim($name)) && !empty(trim($surnames)) && !empty(trim($salary)) && !empty(trim($ycard)) && !empty(trim($rcard)) && !empty(trim($goals)) && !empty(trim($assists))){
-        $sql="UPDATE players SET name=?, surnames=? WHERE id=?";
-        if($stmt = $mysqli->prepare($sql)){
-            $stmt->bind_param("ssi", $param_name,$param_surname,$param_id);
-            $param_name=$name;
-            $param_surname=$surname;
-            $param_id=$id;
-            var_dump($param_id);
-            if($stmt->execute()){
-                // Records updated successfully. Redirect to landing page
-                header("location: index.php");
-                exit();
-            } else{
-                echo "Something went wrong. Please try again later.";
-            }
+        if (!empty($name) && !empty($surnames) && !empty($salary) && !empty($ycard) && !empty($rcard) && !empty($goals) && !empty($assists)){
+            // Error SQL was not correct    
+            $sql = "UPDATE players SET name=?, surname=?, team=?, goals=?, assists=?, salary=?, ycard=?, rcard=? WHERE id=?";
+                $stmt = $mysqli->prepare($sql);
+                //var_dump($stmt);
+                if($stmt = $mysqli->prepare($sql)){
+                    $param_name=$name;
+                    $param_surname=$surnames;
+                    $param_team=$team;
+                    $param_goals=$goals;
+                    $param_assists=$assists;
+                    $param_salary=$salary;
+                    $param_ycard=$ycard;
+                    $param_rcard=$rcard;
+                    $param_id=$id;
+                    $stmt->bind_param("sssiiiiii", $param_name,$param_surname,$param_team,$param_goals,$param_assists,$param_salary,$param_ycard,$param_rcard,$param_id);
+                    //var_dump($param_id);
+                    //var_dump($param_surname);
+                    //var_dump($param_name);
+                    if($stmt->execute()){
+                        // Records updated successfully. Redirect to landing page
+                        header("location: index.php");
+                        exit();
+                    } else{
+                        echo "Something went wrong. Please try again later.";
+                    }
+                }
+                 // Close statement
+            $stmt->close();
         }
-         
-        // Close statement
-      /*   $stmt->close(); */
-    }
     
     // Close connection
     $mysqli->close();
 }else{ 
- if(isset($_GET['id'])&&(!empty(trim($_GET['id'])))){
+ if(isset($_GET['id'])&&!empty(trim($_GET['id']))){
        
       
        $sql="SELECT * FROM players WHERE id=?";
     
         if($stmt = $mysqli->prepare($sql)){
             // Bind variables to the prepared statement as parameters
-    
-    
             $stmt->bind_param("i", $param_id);
             
             // Set parameters
@@ -62,14 +79,14 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                 $res = $stmt->get_result();
                 if($res->num_rows==1){
                     $row = $res->fetch_array();
-                    $names=$row['name'];
-                    $surnames=$row['surname'];
-                    $goals=$row['goals'];
-                    $assists=$row['assists'];
-                    $ycard=$row['ycard'];
-                    $rcard=$row['rcard'];
-                    $salary=$row['salary'];
-                    $team=$row['team'];
+                    $get_name=$row['name'];
+                    $get_surnames=$row['surname'];
+                    $get_goals=$row['goals'];
+                    $get_assists=$row['assists'];
+                    $get_ycard=$row['ycard'];
+                    $get_rcard=$row['rcard'];
+                    $get_salary=$row['salary'];
+                    $get_team=$row['team'];
                 }else{
                     echo "No players found";
                 }   
@@ -112,47 +129,47 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                         <h2>Update Record</h2>
                     </div>
                     <p>Please edit the input values and submit to update the record.</p>
-                    <form action="<?php echo $_SERVER['REQUEST_URI'] ?>" method="post">
+                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])) ?>" method="post">
                         <div class="form-group ">
                             <label>Name</label>
-                            <input type="text" name="name" class="form-control" value="<?php echo $names ?>">
+                            <input type="text" name="name" class="form-control" value="<?php echo $get_name; ?>">
                             <span class="help-block"></span>
                         </div>
                         <div class="form-group ">
                             <label>Surname</label>
-                            <input type="text" name="surname" class="form-control" value="Mendel">
+                            <input type="text" name="surname" class="form-control" value="<?php echo $get_surnames; ?>">
                             <span class="help-block"></span>
                         </div>
                         <div class="form-group ">
                             <label>Team</label>
-                            <input type="text" name="team" class="form-control" value="Barselona">
+                            <input type="text" name="team" class="form-control" value="<?php echo $get_team; ?>">
                             <span class="help-block"></span>
                         </div>
                         <div class="form-group ">
                             <label>goals</label>
-                            <input type="number" name="goals" class="form-control" value="3">
+                            <input type="number" name="goals" class="form-control" value="<?php echo $get_goals; ?>">
                             <span class="help-block"></span>
                         </div>
                         <div class="form-group ">
                             <label>assists</label>
-                            <input type="number" name="assists" class="form-control" value="2">
+                            <input type="number" name="assists" class="form-control" value="<?php echo $get_assists; ?>">
                             <span class="help-block"></span>
                         </div>
                         <div class="form-group ">
                             <label>yellow cards</label>
-                            <input type="number" name="ycard" class="form-control" value="2">
+                            <input type="number" name="ycard" class="form-control" value="<?php echo $get_ycard; ?>">
                             <span class="help-block"></span>
                         </div>
                         <div class="form-group ">
                             <label>red cards</label>
-                            <input type="number" name="rcard" class="form-control" value="2">
+                            <input type="number" name="rcard" class="form-control" value="<?php echo $get_rcard; ?>">
                             <span class="help-block"></span>
                         </div>
                         
                         
                         <div class="form-group ">
                             <label>Salary</label>
-                            <input type="number" name="salary" class="form-control" value="5000">
+                            <input type="number" name="salary" class="form-control" value="<?php echo $get_salary; ?>">
                             <span class="help-block"></span>
                         </div>
                         <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>"/>
